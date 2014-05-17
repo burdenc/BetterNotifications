2c633690e707bc55d8a5f3ce1612b5b292a737c5
Delta <deltaburnt@gmail.com>
2014-05-17 15:00:04 -0500

    Prev/next buttons now hide/show correctly. Some code refactoring.

--- data/js/notifications.js
+++ data/js/notifications.js
@@ -16,14 +16,18 @@ self.port.on('onOpen', function(inputData) {
 		if(this.notifications.length == 0) return;
 		return this;
 	};
+	view['showPrev'] = function() {
+		if(pageIndex != 0) return this;
+		return;
+	};
+	view['showNext'] = function() {
+		if((pageIndex + 1) * 5 < notifications.length) return this;
+		return;
+	};
 	template = inputData['template'];
 	notifications = inputData['notifications'];
 	
-	view['notifications'] = notifications.slice(0,5);
-	
-	document.body.innerHTML = Mustache.render(inputData['template'], view);
-	
-	self.port.emit('setHeight', document.body.offsetHeight);
+	reDraw();
 });
 
 $('body').on('click', 'a', function(event) {
@@ -35,21 +39,21 @@ $('body').on('click', '#clear', function(event) {
 	self.port.emit('clearNotifications');
 	pageIndex = 0;
 	notifications = [];
-	view['notifications'] = notifications;
-	document.body.innerHTML = Mustache.render(template, view);
-	self.port.emit('setHeight', document.body.offsetHeight);
+	reDraw();
 });
 
 $('body').on('click', '#next', function(event) {
 	pageIndex = ((pageIndex+1) * 5 >= notifications.length) ? pageIndex : pageIndex+1;
-	view['notifications'] = notifications.slice(0 + 5*pageIndex, 5*(pageIndex+1));
-	document.body.innerHTML = Mustache.render(template, view);
-	self.port.emit('setHeight', document.body.offsetHeight);
+	reDraw();
 });
 
 $('body').on('click', '#prev', function(event) {
 	pageIndex = (pageIndex == 0) ? 0 : pageIndex-1;
+	reDraw();	
+});
+
+function reDraw() {
 	view['notifications'] = notifications.slice(0 + 5*pageIndex, 5*(pageIndex+1));
 	document.body.innerHTML = Mustache.render(template, view);
 	self.port.emit('setHeight', document.body.offsetHeight);
-});
+}
