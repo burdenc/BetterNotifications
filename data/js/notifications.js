c22f096d3f184ab14d2998cfd196512600db9108
Delta <deltaburnt@gmail.com>
2014-05-16 19:44:32 -0500

    Notif center page functionality added

--- data/js/notifications.js
+++ data/js/notifications.js
@@ -1,22 +1,27 @@
-var view;
+var view = {};
 var template;
+var notifications;
+var pageIndex;
 
 self.port.on('onOpen', function(inputData) {
+	pageIndex = 0;
 	//template = Mustache.parse(inputData['template']);
-	inputData['view']['link2Text'] = function() {
+	view['link2Text'] = function() {
 		var urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
 		return this.content.replace(urlRegex, function(url) {
 			return "<a href='"+url+"'>"+url+"</a>";
 		}, 'g');
 	};
-	inputData['view']['clearButton'] = function() {
+	view['haveNotifications'] = function() {
 		if(this.notifications.length == 0) return;
 		return this;
 	};
-	view = inputData['view'];
 	template = inputData['template'];
+	notifications = inputData['notifications'];
 	
-	document.body.innerHTML = Mustache.render(inputData['template'], inputData['view']);
+	view['notifications'] = notifications.slice(0,5);
+	
+	document.body.innerHTML = Mustache.render(inputData['template'], view);
 	
 	self.port.emit('setHeight', document.body.offsetHeight);
 });
@@ -28,7 +33,23 @@ $('body').on('click', 'a', function(event) {
 
 $('body').on('click', '#clear', function(event) {
 	self.port.emit('clearNotifications');
-	view['notifications'] = [];
+	pageIndex = 0;
+	notifications = [];
+	view['notifications'] = notifications;
+	document.body.innerHTML = Mustache.render(template, view);
+	self.port.emit('setHeight', document.body.offsetHeight);
+});
+
+$('body').on('click', '#next', function(event) {
+	pageIndex = ((pageIndex+1) * 5 >= notifications.length) ? pageIndex : pageIndex+1;
+	view['notifications'] = notifications.slice(0 + 5*pageIndex, 5*(pageIndex+1));
+	document.body.innerHTML = Mustache.render(template, view);
+	self.port.emit('setHeight', document.body.offsetHeight);
+});
+
+$('body').on('click', '#prev', function(event) {
+	pageIndex = (pageIndex == 0) ? 0 : pageIndex-1;
+	view['notifications'] = notifications.slice(0 + 5*pageIndex, 5*(pageIndex+1));
 	document.body.innerHTML = Mustache.render(template, view);
 	self.port.emit('setHeight', document.body.offsetHeight);
 });
